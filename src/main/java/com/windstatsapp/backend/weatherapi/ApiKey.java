785542b5d6b66294d84072d028ba1a06e3bea7ba
MichalStopyra <45542734+MichalStopyra@users.noreply.github.com>
2020-10-03 20:54:05 +0200

    Delete ApiKey.java

--- src/main/java/com/windstatsapp/backend/weatherapi/ApiKey.java
+++
@@ -1,8 +0,0 @@
-package com.windstatsapp.backend.weatherapi;
-
-public class ApiKey {
-        public static String getApiKey () {
-            String link = "<a href=\"https://www.freepik.com/free-photos-vectors/city\">City vector created by freepik - www.freepik.com</a>";
-            return "4b90d7a5baccffb6e73c7a60c1ebe503";
-        }
-}
